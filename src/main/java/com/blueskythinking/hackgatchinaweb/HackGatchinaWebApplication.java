936eea72946cf3faea48335358683f6ec2715f7b
Andrey Tabakov <komdosh@yandex.ru>
2019-05-25 10:28:16 +0300

    init

---
+++ src/main/java/com/blueskythinking/hackgatchinaweb/HackGatchinaWebApplication.java
@@ -0,0 +1,13 @@
+package com.blueskythinking.hackgatchinaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackGatchinaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackGatchinaWebApplication.class, args);
+    }
+
+}
