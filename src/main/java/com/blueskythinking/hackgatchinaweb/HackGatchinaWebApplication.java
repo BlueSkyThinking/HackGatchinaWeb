0c3ca61a81d182b4774a88aadcb066aecc5aa084
Andrey Tabakov <komdosh@yandex.ru>
2019-05-25 15:54:55 +0300

    remove postgres add firebase

--- src/main/java/com/blueskythinking/hackgatchinaweb/HackGatchinaWebApplication.java
+++ src/main/java/com/blueskythinking/hackgatchinaweb/HackGatchinaWebApplication.java
@@ -1,14 +1,33 @@
 package com.blueskythinking.hackgatchinaweb;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+
 @EnableWebMvc
-@EnableJpaRepositories
 @SpringBootApplication
 public class HackGatchinaWebApplication {
+
+    @Value("classpath:config/serviceAccountKey.json")
+    private Resource serviceAccountKey;
+
+    @PostConstruct
+    public void initFirebaseApp() throws IOException {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountKey.getInputStream()))
+                .setDatabaseUrl("https://gatchino-4491c.firebaseio.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HackGatchinaWebApplication.class, args);
